2ded21e252f000dcf2aaeb2e38a5dab2f21ce6f5
Yaseen <yaseensyed77@gmail.com>
2024-06-30 11:28:35 +0530

    adding update test class

---
+++ src/test/java/api/test/testUpdateUser.java
@@ -0,0 +1,12 @@
+package api.test;
+
+import org.testng.annotations.Test;
+
+public class testUpdateUser {
+	
+	@Test
+	public void userUpdate() {
+		System.out.println("Develop a test for update user");
+	}
+
+}
